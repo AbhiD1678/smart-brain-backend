b6ac2053fd6dcaf7bf7bd73f9f9e1e9acb33f05a
AbhiD1678 <104382962+AbhiD1678@users.noreply.github.com>
2022-11-27 15:04:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
-app.use('Access-Control-Allow-Origin':'*');
+
 
 
 app.get('/',cors(),(req,res)=>{res.send('it is working!!')});
