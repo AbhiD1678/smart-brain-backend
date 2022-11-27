d62e8be2fdbb4d2e3ad7d18eeef78ee682efe99a
AbhiD1678 <104382962+AbhiD1678@users.noreply.github.com>
2022-11-27 19:54:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
-
+app.get('/',(req,res)=>{res.send('it is working')});
 
 
 app.get('/cors', (req, res) => {
